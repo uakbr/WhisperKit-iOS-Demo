ea42c28df06ca62b8c1268775d5b2ca403a7d445
Umair Akbar <umair@tesla.com.ai>
2024-12-03 22:47:10 -0600

    Add basic test directory structure with placeholder tests

---
+++ Tests/WhisperKit-iOS-DemoTests/AudioTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import WhisperKit_iOS_Demo
+
+final class AudioTests: XCTestCase {
+    func testExample() {
+        // This is just a placeholder test
+        XCTAssertTrue(true)
+    }
+}
---
+++ Tests/WhisperKit-iOS-DemoTests/TranscriptionTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import WhisperKit_iOS_Demo
+
+final class TranscriptionTests: XCTestCase {
+    func testExample() {
+        // This is just a placeholder test
+        XCTAssertTrue(true)
+    }
+}
